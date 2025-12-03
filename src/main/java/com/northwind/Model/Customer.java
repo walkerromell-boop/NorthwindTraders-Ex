e193bd2c6c95ee3b6637174e9e07677af5e0f8de
Romell Walker <walkerromell@gmail.com>
2025-12-03 12:00:32 -0500

    add customer model

---
+++ src/main/java/com/northwind/Model/Customer.java
@@ -0,0 +1,17 @@
+package com.northwind.Model;
+
+public class Customer {
+   private String customerID;
+   private String companyName;
+   private String contactName;
+   private String contactTitle;
+   private String address;
+   private String city;
+   private String region;
+   private String postalCode;
+   private String country;
+   private String phone;
+   private String fax;
+
+
+}
