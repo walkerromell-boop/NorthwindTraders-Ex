49fe52e9a575a1d62b620c75b369f50e598c7e00
Romell Walker <walkerromell@gmail.com>
2025-12-03 11:47:11 -0500

    add data source

---
+++ src/main/java/com/northwind/Application.java
@@ -0,0 +1,19 @@
+package com.northwind;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+public class Application {
+    public static void main(String[] args) {
+        String username = args[0];
+        String password= args[1];
+        String url = "jdbc:mysql://localhost:3306/northwind";
+
+        BasicDataSource dataSource= new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+    }
+}
